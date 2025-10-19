a362bb5e2d7220f301f17125f22962cdec8a89fb
Arthur R. Oliveira <arolivei@redhat.com>
2025-10-20 00:04:01 +0100

    small fixes

--- embed_image.sh
+++ embed_image.sh
@@ -11,9 +11,9 @@ sha=$(echo "$image" | sha256sum | awk '{ print $1 }')
 #  The LVMS image is "special" because it's a multi-arch manifest, so --all tries copy all platforms and fails. Also using target as sha doesn't work. 
 if [[ $image == *"lvm"* || $image == *"ubi"* ]]; then
    aux=$(echo $image|cut -d\@ -f1)
-   skopeo copy --all $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+   skopeo copy --format v2s2 --all $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
 else
-   skopeo copy $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+   skopeo copy --format v2s2 $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
 fi
 
 echo "$image,$sha" >> /usr/lib/containers/storage/image-list.txt
